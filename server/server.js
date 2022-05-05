7e8b8ef7f5542eec33cf77e6c86235606bdd9f6b
santhoshth <thoomatti.santhosh.w@gmail.com>
2022-05-05 13:39:05 +0530

    POST Users and Products data to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 import express from 'express';
-import products from './data/Products.js';
 import dotenv from 'dotenv';
 import connectDatabase from './config/MongoDB.js';
+import ImportData from './DataImport.js';
 
 dotenv.config();
 connectDatabase();
 const app = express();
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
+// API
+app.use("/api/import", ImportData);
 
-app.get('/api/products/:id', (req, res) => {
-    const productId = parseInt(req.params.id);
-    const product = products.find(p => p.id === productId);
-    res.json(product);
-})
+// app.get('/api/products', (req, res) => {
+//     res.json(products);
+// })
+
+// app.get('/api/products/:id', (req, res) => {
+//     const productId = parseInt(req.params.id);
+//     const product = products.find(p => p.id === productId);
+//     res.json(product);
+// })
 
 app.get('/', (req, res) => {
     res.send("API is running....");
