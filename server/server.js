b2465c51eadd684bb298ec684cae370d9e3bb14b
santhoshth <thoomatti.santhosh.w@gmail.com>
2022-05-05 09:52:18 +0530

    Mongo DB config setup and connected

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import products from './data/Products.js';
 import dotenv from 'dotenv';
+import connectDatabase from './config/MongoDB.js';
 
 dotenv.config();
+connectDatabase();
 const app = express();
-const PORT = process.env.PORT || 1000;
+
 
 app.get('/api/products', (req, res) => {
     res.json(products);
@@ -25,6 +27,8 @@ app.get('/api/products/:category/:id', (req, res) => {
 app.get('/', (req, res) => {
     res.send("API is running....");
 })
+
+const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, () => {
     console.log(`listening in port ${PORT}`);
