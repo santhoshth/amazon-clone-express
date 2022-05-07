f198fd21eb4b4a1652f3b930cd946dfe9ab6ed82
santhoshth <thoomatti.santhosh.w@gmail.com>
2022-05-07 17:30:01 +0530

    User Register API

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -28,6 +28,39 @@ userRoute.post("/login", asyncHandler(
     }
 ));
 
+// REGISTER
+userRoute.post("/", asyncHandler(
+    async (req, res) => {
+        const { name, email, password } = req.body;
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            res.status(400);
+            throw new Error("User already exists");
+        }
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+        });
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                jwtToken: generateToken(user._id),
+
+            });
+        } else {
+            res.status(400);
+            throw new Error("Invalid User Data")
+        }
+    }
+));
+
 // USER PROFILE
 userRoute.get("/profile", protect, asyncHandler(
     async (req, res) => {
