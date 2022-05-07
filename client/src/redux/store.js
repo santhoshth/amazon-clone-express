18714e74d695e55573493a1ef63d697c65799613
santhoshth <thoomatti.santhosh.w@gmail.com>
2022-05-07 17:47:52 +0530

    Login Redux setup

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,20 +3,31 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productDetailsReducer, productListReducer } from './reducers/ProductReducers';
 import { cartReducer } from './reducers/CartReducers';
+import { userLoginReducer } from './reducers/UserReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 });
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
     : [];
 
+// LOGIN
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null;
+
+
 const initialState = {
     cart: {
         cartItems: cartItemsFromLocalStorage,
+    },
+    userLogin: {
+        userInfo: userInfoFromLocalStorage
     },
 }
 
