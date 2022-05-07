7a740f7f1ff361b9f86aca8a675b1b21944e25d4
santhoshth <thoomatti.santhosh.w@gmail.com>
2022-05-07 09:11:19 +0530

    Login API, User Profile API with Json Web Tokens (JWT)

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -2,6 +2,7 @@ import asyncHandler from 'express-async-handler';
 import express from 'express';
 import User from './../models/UserModel.js';
 import generateToken from './../utils/generateTokens.js';
+import protect from '../middleware/AuthMiddleware.js';
 
 const userRoute = express.Router();
 
@@ -16,13 +17,32 @@ userRoute.post("/login", asyncHandler(
                 _id: user._id,
                 name: user.name,
                 email: user.email,
-                isAdmin: user.Admin,
+                isAdmin: user.isAdmin,
                 jwtToken: generateToken(user._id),
                 createdAt: user.createdAt,
             });
         } else {
             res.status(401);
             throw new Error("Invalid Email or Password");
+        }
+    }
+));
+
+// USER PROFILE
+userRoute.get("/profile", protect, asyncHandler(
+    async (req, res) => {
+        const user = await User.findById(req.user._id);
+        if (user) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                createdAt: user.createdAt,
+            })
+        } else {
+            res.status(404);
+            throw new Error("User not found");
         }
     }
 ));
